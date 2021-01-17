37d6eda0d1061707f3fb1232dd7ac16d8be671f0
Tatsuya Yamanoi <71238768+TatsuyaYamanoi-sys@users.noreply.github.com>
2021-01-18 01:18:53 +0900

    フォームの不具合とスタイルを修正

--- php/form.php
+++ php/form.php
@@ -32,9 +32,34 @@ function h($str){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/modules/reset.css">
+    <link rel="stylesheet" href="../style/include/form.css">
     <title>お問い合わせ</title>
 </head>
 <body>
+
+    <header class="page-header">
+        <div class="page-header__inner">
+            <div class="page-header__logo">
+                <a class="logo" href="../index.html">
+                    <img src="../img/logo/y's-teck_logo_wh2.svg" alt="logo-header">
+                </a>
+            </div>
+            <!-- <nav class="page-header__nav">
+                <ul class="page-header__nav-ul">
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#feature">FEATURE</a></li>
+                    <li><a href="#profile">PROFILE</a></li>
+                    <li><a class="btn-icon_mail" href="#contact">CONTACT</a></li>
+                </ul>
+            </nav>
+            <button class="page-header__mobile-menu-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button> -->
+        </div>
+    </header>    <!-- 後で共通コンポーネント化 -->
     <?php if($pageFlag === 0 ) : ?>
         <?php 
             if(!isset($_SESSION['csrfToken'])) {
@@ -43,105 +68,119 @@ function h($str){
             }
             $token = $_SESSION['csrfToken'];
         ?>
-
         <form method="POST" action="form.php">
-            氏名
-            <input type="text" name="your_name" value=
-            "<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']); } ?>">
-            <br>
-            性別
-            <input type="radio" name="gender" value="1"
-            <?php if(!empty($_POST['gender']) && $_POST['gender'] === "1"){echo "checked";}?>>男性
-            <input type="radio" name="gender" value="2"
-            <?php if(!empty($_POST['gender']) && $_POST['gender'] === "2"){echo "checked";}?>>女性
-            <br>
-            メールアドレス
-            <input type="email" name="email" value=
-            "<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>">
-            <br>
-            年齢
-            <select name="age">
-                <option value="">選択してください</option>
-                <option value="1"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "1"){echo "selected";}?>>~19歳</option>
-                <option value="2"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "2"){echo "selected";}?>>20歳~29歳</option>
-                <option value="3"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "3"){echo "selected";}?>>30歳~39歳</option>
-                <option value="4"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "4"){echo "selected";}?>>40歳~49歳</option>
-                <option value="5"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "5"){echo "selected";}?>>50歳~59歳</option>
-                <option value="6"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "6"){echo "selected";}?>>60歳~</option>
-                <option value="7"
-                <?php if(!empty($_POST['age']) && $_POST['age'] === "7"){echo "selected";}?>>70歳~</option>
-            </select>
-            <br>
-            お問い合わせ種別
-            <input type="checkbox" name="contact_type[]" value="ご依頼">ご依頼
-            <input type="checkbox" name="contact_type[]" value="求人">求人
-            <input type="checkbox" name="contact_type[]" value="その他">その他
-            <br>
-            <textarea name="contact" id="" cols="30" rows="10"><?php if(!empty($_POST['contact'])){echo $_POST['contact'];}?></textarea>
-            <br>
-            <input type="submit" name="btn_confirm" value="入力内容確認">
+            <div class="grid">
+                <label class="grid_left" for="your_name">氏名</label>
+                <input type="text" name="your_name" value=
+                "<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']); } ?>">
+                <br>
+                <label class="grid_left" for="gender">性別</label>
+                <label class="grid_right"><input type="radio" name="gender" class="grid_right" value="1"
+                <?php if(!empty($_POST['gender']) && $_POST['gender'] === "1"){echo "checked";}?>>男性</label>
+                <label class="grid_right"><input type="radio" name="gender" class="grid_right" value="2"
+                <?php if(!empty($_POST['gender']) && $_POST['gender'] === "2"){echo "checked";}?>>女性</label>
+                <br>
+                <label class="grid_left" for="email">メールアドレス</label>
+                <input type="email" name="email" value=
+                "<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>">
+                <br>
+                <label class="grid_left" for="age">年齢</label>
+                <select name="age">
+                    <option value="">選択してください</option>
+                    <option value="1"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "1"){echo "selected";}?>>~19歳</option>
+                    <option value="2"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "2"){echo "selected";}?>>20歳~29歳</option>
+                    <option value="3"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "3"){echo "selected";}?>>30歳~39歳</option>
+                    <option value="4"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "4"){echo "selected";}?>>40歳~49歳</option>
+                    <option value="5"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "5"){echo "selected";}?>>50歳~59歳</option>
+                    <option value="6"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "6"){echo "selected";}?>>60歳~</option>
+                    <option value="7"
+                    <?php if(!empty($_POST['age']) && $_POST['age'] === "7"){echo "selected";}?>>70歳~</option>
+                </select>
+                <br>
+                <label class="grid_left" for="contact_type[]">お問い合わせ種別</label>
+                <label class="grid_right"><input type="checkbox" name="contact_type[]" value="ご依頼"
+                <?php if(!empty($_POST['contact_type']) && preg_match("/ご依頼/", $_POST['contact_type'])){echo "checked";}?>>ご依頼</label>
+                <label class="grid_right"><input type="checkbox" name="contact_type[]" value="求人"
+                <?php if(!empty($_POST['contact_type']) && preg_match("/求人/", $_POST['contact_type'])){echo "checked";}?>>求人</label>
+                <label class="grid_right"><input type="checkbox" name="contact_type[]" value="その他 "
+                <?php if(!empty($_POST['contact_type']) && preg_match("/その他/", $_POST['contact_type'])){echo "checked";}?>>その他</label>
+                <br>
+                <label class="grid_left" for="contact">お問い合わせ内容</label>
+                <textarea class="contact" name="contact" cols="30" rows="10"><?php if(!empty($_POST['contact'])){echo $_POST['contact'];}?></textarea>
+                <br>
+            </div>
+            <div class="submit-area">
+                <input type="submit" name="btn_confirm" value="入力内容確認">
+            </div>
             <br><!-- トップページでcsrfTokenをunset？ -->
-            <a href="#">トップページに戻る</a>
+            <a href="../index.html" class="link_top">トップページに戻る</a>
             <input type="hidden" name="csrf" value="<?php echo $token; ?>">
-
         </form>
     <?php endif; ?>
 
     <?php if($pageFlag === 1 && $_POST['csrf'] === $_SESSION['csrfToken']) : ?>
    
-    <!-- csrfでtokenが一致しなかった時等の処理を行いたい場合以下のように分けて分岐処理。 -->
-    <?php /*if($pageFlag === 1 ) : */?>
-    <?php /*if($_POST['csrf'] === $_SESSION['csrfToken']) : */?>
-    <?php /*elseif() : */?>
-    <?php /*else : */?>
-    <?php /*endif; */?>
+        <!-- csrfでtokenが一致しなかった時等の処理を行いたい場合以下のように分けて分岐処理。 -->
+        <?php /*if($pageFlag === 1 ) : */?>
+        <?php /*if($_POST['csrf'] === $_SESSION['csrfToken']) : */?>
+        <?php /*elseif() : */?>
+        <?php /*else : */?>
+        <?php /*endif; */?>
 
         <form method="POST" action="form.php">
-            氏名
-            <?php echo $_POST["your_name"]; ?>
-            <br>
-            性別
-            <?php 
-                if(!empty($_POST['gender']) && $_POST['gender'] === '1'){echo "男性";}
-                if(!empty($_POST['gender']) && $_POST['gender'] === '2'){echo "女性";}
-            ?>
-            <br>
-            メールアドレス
-            <?php echo $_POST['email']; ?>
-            <br>
-            年齢
-            <?php  
-                if($_POST['age'] === "1"){echo "~19歳";}
-                if($_POST['age'] === "2"){echo "20歳~29歳";}
-                if($_POST['age'] === "3"){echo "30歳~39歳";}
-                if($_POST['age'] === "4"){echo "40歳~49歳";}
-                if($_POST['age'] === "5"){echo "50歳~59歳";}
-                if($_POST['age'] === "6"){echo "60歳~";}
-                if($_POST['age'] === "7"){echo "70歳~";}
-            ?>
-            <br>
-            お問い合わせ種別
-            <?php if(!empty($_POST['contact_type'])){ 
-                foreach($_POST['contact_type'] as $ctValue){
-                echo $ctValue." "; }}?>
-            <br>
-            お問い合わせ内容
-            <?php if(!empty($_POST['contact'])){echo $_POST['contact'];}?>
-            <br>
-
-            <input type="submit" name="btn_back" value="戻る">
-            <input type="submit" name="btn_submit" value="送信する">
+            <div class="grid">
+                <p class="grid_left">氏名</p>
+                <?php echo h($_POST["your_name"]); ?>
+                <br>
+                <p class="grid_left">性別</p>
+                <?php 
+                    if(!empty($_POST['gender']) && $_POST['gender'] === '1'){echo "男性";}
+                    if(!empty($_POST['gender']) && $_POST['gender'] === '2'){echo "女性";}
+                ?>
+                <br>
+                <p class="grid_left">メールアドレス</p>
+                <?php echo $_POST['email']; ?>
+                <br>
+                <p class="grid_left">年齢</p>
+                <?php  
+                    if($_POST['age'] === "1"){echo "~19歳";}
+                    if($_POST['age'] === "2"){echo "20歳~29歳";}
+                    if($_POST['age'] === "3"){echo "30歳~39歳";}
+                    if($_POST['age'] === "4"){echo "40歳~49歳";}
+                    if($_POST['age'] === "5"){echo "50歳~59歳";}
+                    if($_POST['age'] === "6"){echo "60歳~";}
+                    if($_POST['age'] === "7"){echo "70歳~";}
+                ?>
+                <br>
+                <p class="grid_left">お問い合わせ種別</p>
+                <?php if(!empty($_POST['contact_type'])){
+                    $ctValues = ""; 
+                    foreach($_POST['contact_type'] as $ctValue){ 
+                    $ctValues .= $ctValue." ";
+                    }
+                    if(!empty($ctValues)){ 
+                    echo $ctValues; 
+                }}?>
+                <br>
+                <p class="grid_left">お問い合わせ内容</p>
+                <?php if(!empty($_POST['contact'])){echo $_POST['contact'];}?>
+                <br>
+            </div>
+            <div class="submit-area">
+                <input type="submit" name="btn_back" value="戻る">
+                <input type="submit" name="btn_submit" value="送信する">
+            </div>
             <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
             <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
             <input type="hidden" name="gender" value="<?php echo $_POST['gender']; ?>">
             <input type="hidden" name="age" value="<?php echo $_POST['age']; ?>">
-            <input type="hidden" name="contact_type" value="<?php echo $ctValue." "; ?>">
+            <input type="hidden" name="contact_type" value="<?php echo $ctValues; ?>">
             <input type="hidden" name="contact" value="<?php echo $_POST['contact']; ?>">
             <input type="hidden" name="csrf" value="<?php echo $_POST['csrf']; ?>">
 
@@ -149,10 +188,13 @@ function h($str){
     <?php endif; ?>
 
     <?php if($pageFlag === 2 && $_POST['csrf'] === $_SESSION['csrfToken']) : ?>
-        <p>送信が完了しました。</p>
+        <p class="send-msg">送信が完了しました。</p>
         <?php unset($_SESSION['csrfToken']); ?>
-        <a href="http://localhost/php_test/form/form.php">トップページに戻る</a>
+        <a href="../index.html" class="link_top">トップページに戻る</a>
     <?php endif; ?>
     
+    <footer>
+
+    </footer>
 </body>
 </html>
